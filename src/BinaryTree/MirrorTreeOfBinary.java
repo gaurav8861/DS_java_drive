151e89a489790582df8c35016d9e793f667e6f18
Gaurav Kumar <gkumar@cray.com>
2020-03-28 18:01:35 +0530

    adding mirror tree

---
+++ src/BinaryTree/MirrorTreeOfBinary.java
@@ -0,0 +1,46 @@
+package BinaryTree;
+
+public class MirrorTreeOfBinary {
+
+	public static void main(String[] args) {
+		Node root = new Node(1);
+		root.leftNode = new Node(3);
+		root.rightNode = new Node(2);
+		root.rightNode.leftNode = new Node(5);
+		root.rightNode.rightNode = new Node(4);
+		
+		System.out.println("Before mirror");
+		inOrderTraversal(root);
+		System.out.println();
+		
+		System.out.println("Mirror of Binary Tree");
+		mirrorOfBinaryTree(root);
+		
+		System.out.println("After mirror");
+		inOrderTraversal(root);
+	}
+
+	private static void inOrderTraversal(Node root) {
+		if(root == null) {
+			return;
+		} else {
+			inOrderTraversal(root.leftNode);
+			System.out.print(root.data + " ");
+			inOrderTraversal(root.rightNode);
+		}
+	}
+
+	private static void mirrorOfBinaryTree(Node root) {
+		if(root == null) {
+			return;
+		}else {
+			mirrorOfBinaryTree(root.leftNode);
+			mirrorOfBinaryTree(root.rightNode);
+			Node temp = root.leftNode;
+			root.leftNode = root.rightNode;
+			root.rightNode = temp;
+			
+		}
+	}
+
+}
